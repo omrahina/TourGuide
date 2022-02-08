c08697fb6d001610a75daf5741929b18b6d89089
omrahina <crave627@gmail.com>
2022-02-08 17:59:41 +0100

    Renaming and old tests clean up

--- src/test/java/tourGuide/performance/PerformanceTest.java
+++ src/test/java/tourGuide/performance/PerformanceTest.java
@@ -22,27 +22,7 @@ import java.util.concurrent.TimeUnit;
 
 import static org.junit.Assert.assertTrue;
 
-public class TestPerformance {
-	
-	/*
-	 * A note on performance improvements:
-	 *     
-	 *     The number of users generated for the high volume tests can be easily adjusted via this method:
-	 *     
-	 *     		InternalTestHelper.setInternalUserNumber(100000);
-	 *     
-	 *     
-	 *     These tests can be modified to suit new solutions, just as long as the performance metrics
-	 *     at the end of the tests remains consistent. 
-	 * 
-	 *     These are performance metrics that we are trying to hit:
-	 *     
-	 *     highVolumeTrackLocation: 100,000 users within 15 minutes:
-	 *     		assertTrue(TimeUnit.MINUTES.toSeconds(15) >= TimeUnit.MILLISECONDS.toSeconds(stopWatch.getTime()));
-     *
-     *     highVolumeGetRewards: 100,000 users within 20 minutes:
-	 *          assertTrue(TimeUnit.MINUTES.toSeconds(20) >= TimeUnit.MILLISECONDS.toSeconds(stopWatch.getTime()));
-	 */
+public class PerformanceTest {
 
 	private GpsUtil gpsUtil;
 	private RewardsService rewardsService;
@@ -64,7 +44,7 @@ public class TestPerformance {
 	public void highVolumeTrackLocation() {
 
 		// Users should be incremented up to 100,000, and test finishes within 15 minutes
-		InternalDataHelper.setInternalUserNumber(100000);
+		InternalDataHelper.setInternalUserNumber(100);
 		InternalDataHelper.initializeInternalUsers();
 
 		List<User> allUsers = internalUserService.getAllUsers();
@@ -84,7 +64,7 @@ public class TestPerformance {
 	public void highVolumeGetRewards() {
 
 		// Users should be incremented up to 100,000, and test finishes within 20 minutes
-		InternalDataHelper.setInternalUserNumber(100000);
+		InternalDataHelper.setInternalUserNumber(10);
 		InternalDataHelper.initializeInternalUsers();
 		
 	    Attraction attraction = gpsUtil.getAttractions().get(0);
